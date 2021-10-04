40a440b2cbbf96ef0b315631ea8874a47998d2b2
Th3Dilli <th3dilli@gmx.at>
2021-10-04 11:03:29 +0200

    testing with generation structures

--- src/Th3Dungeon.cs
+++ src/Th3Dungeon.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
-
+using Vintagestory.ServerMods;
 
 namespace Th3Dungeon
 {
@@ -15,6 +19,7 @@ namespace Th3Dungeon
     private IBlockAccessor _worldBlockAccessor;
 
     private int _chunkSize;
+
     private int _creativeBlockId;
 
     public override bool ShouldLoad(EnumAppSide side)
@@ -38,7 +43,49 @@ namespace Th3Dungeon
       _api.Event.ChunkColumnGeneration(OnChunkColumnGeneration, EnumWorldGenPass.TerrainFeatures, "standard");
 
 
+
       _creativeBlockId = _api.WorldManager.GetBlockId(new AssetLocation("game:creativeblock-0"));
+
+      _api.RegisterCommand("spawnstruct", "spawn all structures", string.Empty, OnSpawnStructures);
+    }
+
+    private void OnSpawnStructures(IServerPlayer player, int groupId, CmdArgs args)
+    {
+      player.SendMessage(GlobalConstants.GeneralChatGroup, "loaded assets", EnumChatType.CommandSuccess);
+      Dictionary<AssetLocation, BlockSchematicStructure> assets = _api.Assets.GetMany<BlockSchematicStructure>(_api.Logger, "worldgen/schematics");
+      Mod.Logger.VerboseDebug("loaded assets: " + assets.Count);
+      BlockPos pos = new BlockPos();
+      int x = 0, z = 0;
+      int sx = player.Entity.Pos.AsBlockPos.X - 200;
+      int sz = player.Entity.Pos.AsBlockPos.Z - 200;
+      pos.Y = player.Entity.Pos.AsBlockPos.Y;
+      int offset = args.PopInt(20) ?? 20;
+      int rowmax = args.PopInt(400) ?? 400;
+
+      pos.X = sx;
+      pos.Z = sz;
+      int sizeX = offset, sizeZ = offset;
+      foreach (KeyValuePair<AssetLocation, BlockSchematicStructure> structure in assets)
+      {
+        // Mod.Logger.VerboseDebug($"struc: {structure.Key.GetName()}");
+        structure.Value.Init(_worldBlockAccessor);
+        structure.Value.Place(_worldBlockAccessor, _api.World, pos, true);
+        sizeX = Math.Max(sizeX, structure.Value.SizeX + 2);
+        sizeZ = Math.Max(sizeZ, structure.Value.SizeZ + 2);
+        if (x >= rowmax)
+        {
+          x = 0;
+          z += Math.Max(offset, sizeZ);
+          sizeX = offset;
+          sizeZ = offset;
+        }
+        else
+        {
+          x += Math.Max(offset, sizeX);
+        }
+        pos.X = sx + x;
+        pos.Z = sz + z;
+      }
     }
 
     private void OnWorldGenBlockAccessor(IChunkProviderThread chunkProvider)
@@ -52,7 +99,6 @@ namespace Th3Dungeon
       {
         int x = _chunkSize / 2;
         int z = x;
-
 
         int chunkY = y % _chunkSize;
         int index3d = (((chunkY * _chunkSize) + z) * _chunkSize) + x;
