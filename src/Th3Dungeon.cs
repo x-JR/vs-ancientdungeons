83c911f3b3bf43d1009c61f4125332d248bf476a
Th3Dilli <th3dilli@gmx.at>
2021-11-20 16:28:05 +0100

    add walls to structures

--- src/Th3Dungeon.cs
+++ src/Th3Dungeon.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
