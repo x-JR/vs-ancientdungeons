4faa542b3beaa8876b073ce10f6a6fe389de407f
Th3Dilli <th3dilli@gmx.at>
2024-03-15 19:48:57 +0100

    add launch project for hot reloading

---
+++ Launcher/Launcher.cs
@@ -0,0 +1,19 @@
+﻿using Vintagestory.Client;
+
+namespace Launcher;
+
+public static class Launcher
+{
+    public static void Main(string[] args)
+    {
+        var libs = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lib");
+        var files = Directory.GetFiles(libs, "*.so*");
+        foreach (var file in files)
+        {
+            var native = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileName(file));
+            if(!File.Exists(native))
+                File.Copy(file,native);
+        }
+        ClientProgram.Main(args);
+    }
+}
